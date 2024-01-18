40d844be3d443e3709d37dbcc6c7688940f92a44
luanaftrindade <luanatrindade.dev@gmail.com>
2024-01-18 21:58:30 +0000

    Now the phone number to make a reservation only accepts numbers and have one hook to update the phone number

--- src/components/formReservation/PhoneNumberInput.jsx
+++ src/components/formReservation/PhoneNumberInput.jsx
@@ -1,12 +1,27 @@
-export function PhoneNumberInput({ value, onChange }) {
+import React, { useState } from "react";
+
+// regex to match numbers between 1 and 10 digits long
+const validPhoneNumber = /^\d{1,10}$/;
+
+export function PhoneNumberInput() {
+  // declare current state and state setter
+  const [phone, setPhone] = useState("");
+  const handleChange = ({ target }) => {
+    const newPhone = target.value;
+    const isValid = validPhoneNumber.test(newPhone);
+    if (isValid) {
+      setPhone(newPhone);
+    }
+  };
+
   return (
     <label>
       Enter your phone number:
       <input
         type="number"
         name="phoneNumber"
-        value={value}
-        onChange={onChange}
+        value={phone}
+        onChange={handleChange}
       />
     </label>
   );
